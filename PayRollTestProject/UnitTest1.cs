1cb9c0546b1e2179a25a7244e7e3b718118531c2
madhurtyagi <38392631+madhurtyagi@users.noreply.github.com>
2019-06-11 12:48:34 +0530

    Commiy21

--- PayRollTestProject/UnitTest1.cs
+++ PayRollTestProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayrollLibrary;
 
 namespace PayRollTestProject
 {
@@ -7,8 +8,90 @@ namespace PayRollTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckFileTestMethod()
         {
+            //Arrange Test
+            bool Result;
+
+            //Act Test
+            Result =PayrollOperations.testFilePath(@"E:\testfile.txt");
+
+            //Assert Test
+            Assert.AreEqual(true, Result);
+        }
+
+        [TestMethod]
+        public void ReadFileTestMethod()
+        {
+            //Arrange Test
+            string[] Result;
+
+            //Act Test
+            Result = PayrollOperations.readTextFile(@"E:\testfile.txt");
+
+            //Assert Test
+            Assert.AreEqual(4, Result.Length);
+        }
+
+        [TestMethod]
+        public void ReadLinesTestMethod()
+        {
+            //Arrange Test
+            string Result;
+            string ExpResult;
+            string Line = "102\tankur\t11-07-2016\t40000\\r\\n";
+
+
+            //Act Test
+            Result = PayrollOperations.readLines(Line);
+            ExpResult = PayrollOperations.readLines(Line);
+
+            //Assert Test
+            Assert.AreEqual(ExpResult, Result);
+        }
+
+
+        [TestMethod]
+        public void CheckDateTestMethod()
+        {
+            //Arrange Test
+            bool Result;
+
+            //Act Test
+            Result = PayrollOperations.compareDate("10-12-2017");
+
+            //Assert Test
+            Assert.AreEqual(true, Result);
+        }
+
+        [TestMethod]
+        public void SalaryIncreamentTestMethod()
+        {
+            //Arrange Test
+            string salary = "30000";
+            double Result;
+
+            //Act Test
+            Result = PayrollOperations.calculateNewSalary(salary);
+
+            //Assert Test
+            Assert.AreEqual(33000, Result);
+        }
+
+        [TestMethod]
+        public void WriteFileTestMethod()
+        {
+            //Arrange Test
+            string filepath = @"E:\testfile.txt";
+            string Result, ExpResult;
+
+            //Act Test
+            ExpResult = "101\tsahil\t10-12-2017\t30000\t33000\r\n102\tankur\t11-07-2016\t40000\t44000\r\n102\tankur\t21-07-2018\t40000\t40000\r\n";
+            Result = PayrollOperations.writeToFile(filepath);
+
+            //Assert Test
+            Assert.AreEqual(ExpResult, Result);
         }
     }
+
 }
